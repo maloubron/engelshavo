c6ff8307c80144e518e0c00d9823a8001ea809e3
MalouBron <m.bron.3@student.rug.nl>
2019-07-08 11:23:17 +0200

    initial commit

---
+++ HAVO Engels eindexamen/HAVO Engels eindexamen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HAVO Engels eindexamen
+//
+//  Created by Malou Bron on 08/07/2019.
+//  Copyright © 2019 Malou Bron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HAVO Engels eindexamen/HAVO Engels eindexamenTests/HAVO_Engels_eindexamenTests.swift
@@ -0,0 +1,34 @@
+//
+//  HAVO_Engels_eindexamenTests.swift
+//  HAVO Engels eindexamenTests
+//
+//  Created by Malou Bron on 08/07/2019.
+//  Copyright © 2019 Malou Bron. All rights reserved.
+//
+
+import XCTest
+@testable import HAVO_Engels_eindexamen
+
+class HAVO_Engels_eindexamenTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
